f0cf7be7731f0c0961c5d3cb0be0aed6b4341580
je-pa <jieun0502@gmail.com>
2021-11-02 12:58:24 +0900

    stack

--- src/com/jepa/bj/Main.java
+++ src/com/jepa/bj/Main.java
@@ -3,7 +3,6 @@ package com.jepa.bj;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
     }
 }
---
+++ src/com/jepa/bj/basic1/b10828_Stack.java
@@ -0,0 +1,37 @@
+package com.jepa.bj.basic1;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class b10828_Stack {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int a = scan.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<a ; i++){
+            switch(scan.next()){
+                case "push":
+                    list.add(scan.nextInt());
+                    break;
+                case "size":
+                    sb.append(list.size()).append('\n');
+                    break;
+                case "empty":
+                    sb.append(list.isEmpty()?1:0).append('\n');
+                    break;
+                case "top":
+                    sb.append(list.isEmpty()?-1:list.get(list.size()-1)).append('\n');
+                    break;
+                case "pop":
+                    if(list.isEmpty()){
+                        sb.append(-1).append('\n');
+                    }else{
+                        sb.append(list.get(list.size()-1)).append('\n');
+                        list.remove(list.size()-1);
+                    }
+                    break;
+            }
+        }System.out.println(sb);
+    }
+}
