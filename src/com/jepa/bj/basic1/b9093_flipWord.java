910d3a90b593d4748bdb2acf9a628d07c1e5f8e9
je-pa <jieun0502@gmail.com>
2021-11-03 11:09:10 +0900

    단어 뒤집기

---
+++ src/com/jepa/bj/basic1/b9093_flipWord.java
@@ -0,0 +1,23 @@
+package com.jepa.bj.basic1;
+import java.util.Scanner;
+public class b9093_flipWord {
+        public static void main(String[] args){
+            Scanner scan = new Scanner(System.in);
+            int n = scan.nextInt();
+            StringBuilder sb = new StringBuilder();
+            scan.nextLine();
+            for(int i=0 ; i<n ; i++){
+                String str = scan.nextLine();
+                StringBuilder sb2 = new StringBuilder();
+                for(int j=0 ; j<str.length() ;j++){
+                    char c = str.charAt(j);
+                    if(c!=' '){
+                        sb2.insert(0,c);
+                    }else{
+                        sb.append(sb2).append(c);
+                        sb2.setLength(0);
+                    }
+                }sb.append(sb2).append('\n');
+            }System.out.println(sb);
+        }
+}
