59941f60261009226f33deacf69fb76718526f2a
je-pa <jieun0502@gmail.com>
2021-11-04 11:49:31 +0900

    괄호

---
+++ src/com/jepa/bj/basic1/b9012_bracket.java
@@ -0,0 +1,20 @@
+package com.jepa.bj.basic1;
+import java.util.Scanner;
+/*9012 괄호*/
+public class b9012_bracket {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0 ; i<n ; i++){
+            String str = sc.next();
+            int result=0;
+            for(int j=0 ; j<str.length() ; j++){
+                result += str.charAt(j)=='(' ? 1 : -1 ;
+                if(result<0) break;
+            }
+            sb.append(result==0?"YES":"NO").append('\n');
+        }
+        System.out.print(sb);
+    }
+}
