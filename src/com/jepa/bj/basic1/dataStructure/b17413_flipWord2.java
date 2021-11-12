d0f4af6de9957746fb8b1f2de9410b60f31129d6
je-pa <jieun0502@gmail.com>
2021-11-13 00:42:30 +0900

    단어 뒤집기2

---
+++ src/com/jepa/bj/basic1/dataStructure/b17413_flipWord2.java
@@ -0,0 +1,37 @@
+package com.jepa.bj.basic1.dataStructure;
+
+import java.util.Scanner;
+
+public class b17413_flipWord2 {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        StringBuilder word = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        boolean open = false;
+
+        for(int i=0 ; i<str.length() ; i++ ){
+            char c = str.charAt(i);
+            if(open){
+                if(c!='>'){
+                    word.append(c);
+                }else{
+                    open = false;
+                    result.append(word.append(c));
+                    word.setLength(0);
+                }
+            }else if(c=='<'){
+                open = true;
+                word.append(str.charAt(i));
+            }else{
+                if(c!=' '){
+                    word.insert(0,c);
+                }else{
+                    result.append(word.append(c));
+                    word.setLength(0);
+                }
+            }
+        }
+        System.out.println(result.append(word));
+    }
+}
