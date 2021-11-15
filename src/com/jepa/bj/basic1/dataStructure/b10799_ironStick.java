dd85a0f6c615b0911606ae3f960809be36fda9cd
je-pa <jieun0502@gmail.com>
2021-11-15 16:48:25 +0900

    쇠막대기

---
+++ src/com/jepa/bj/basic1/dataStructure/b10799_ironStick.java
@@ -0,0 +1,28 @@
+package com.jepa.bj.basic1.dataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b10799_ironStick {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int n = 0;
+        int sum = 0;
+        for(int i=0 ; i<str.length() ; i++){
+            if(str.charAt(i)=='('){
+                if(str.charAt(i+1)=='('){
+                    n++;
+                }else{
+                    sum+=n;
+                    i++;
+                }
+            }else{
+                sum+=1;
+                n--;
+            }
+        }
+        System.out.println(sum);
+    }
+}
