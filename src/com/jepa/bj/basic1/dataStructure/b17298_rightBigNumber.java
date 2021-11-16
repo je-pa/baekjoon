68287131749bec847312474677fb29d139dcb7ca
je-pa <jieun0502@gmail.com>
2021-11-16 11:38:26 +0900

    오큰수

---
+++ src/com/jepa/bj/basic1/dataStructure/b17298_rightBigNumber.java
@@ -0,0 +1,35 @@
+package com.jepa.bj.basic1.dataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class b17298_rightBigNumber {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] a = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        for(int i=1 ; i<N ; i++){
+            if(a[st.peek()]>=a[i]){
+                st.push(i);
+            }else{
+                while(!st.empty()&&a[st.peek()]<a[i]){
+                    a[st.pop()]=a[i];
+                }
+                st.push(i);
+            }
+        }
+        while(!st.empty()){
+            a[st.pop()]=-1;
+        }
+        for(int i=0; i<N ; i++){
+            sb.append(a[i]+" ");
+        }
+        System.out.println(sb);
+    }
+}
