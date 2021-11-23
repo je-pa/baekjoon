8dcc543409703b047c5fdbfd56b6b4a9c718bcba
je-pa <jieun0502@gmail.com>
2021-11-23 10:38:53 +0900

    클래스 이름 바꿈

--- src/com/jepa/bj/basic1/dataStructure1/b10809_numberOfAlphabets.java
+++ src/com/jepa/bj/basic1/dataStructure1/b10809_numberOfAlphabets.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class numberOfAlphabets {
+public class b10809_numberOfAlphabets {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
