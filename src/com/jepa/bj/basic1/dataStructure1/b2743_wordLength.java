38e0afda5545bac37f2589111dab2f4461ae3917
je-pa <jieun0502@gmail.com>
2021-11-24 11:21:37 +0900

    단어 길이 재기

---
+++ src/com/jepa/bj/basic1/dataStructure1/b2743_wordLength.java
@@ -0,0 +1,12 @@
+package com.jepa.bj.basic1.dataStructure1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b2743_wordLength {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+}
